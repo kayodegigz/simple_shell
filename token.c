fb3d0df639bd93bb2383353361d7c59546b32702
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-05-19 00:06:24 +0100

    token

---
+++ token.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * token - token a buffer with a delimiter
+ * @buffer: buffer to tokenize
+ * @delimiter: delimiter to tokenize along
+ *
+ * Return: pointer to an array of pointers to the tokens
+ */
+char **token(char *buffer, char *delimiter)
+{
+	char **tokens = NULL;
+	size_t i = 0, mcount = 10;
+
+	if (buffer == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * mcount);
+	if (tokens == NULL)
+	{
+		perror("Fatal Error");
+		return (NULL);
+	}
+	while ((tokens[i] = new_strtokk(buffer, delimiter)) != NULL)
+	{
+		i++;
+		if (i == mcount)
+		{
+			tokens = _reallocc(tokens, &mcount);
+			if (tokens == NULL)
+			{
+				perror("Fatal Error");
+				return (NULL);
+			}
+		}
+		buffer = NULL;
+	}
+	return (tokens);
+}
